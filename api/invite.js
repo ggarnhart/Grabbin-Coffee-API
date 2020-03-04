81f46b646f5193ef812d4a3a99be92e57b8ab73b
Greg Garnhart <gegarnhart@email.wm.edu>
2020-03-03 21:42:21 -0500

    updated invite js api to fully integrate with app post request

--- api/invite.js
+++ api/invite.js
@@ -6,8 +6,10 @@ sgMail.setApiKey(process.env.SENDGRID_GRABBIN_COFFEE_KEY);
 // it is very easy to understand, but is not coded very intelligently
 // but it works, it's easy, and it's fun!
 async function sendInvite(invite_information) {
+  var countFound = parseInt(invite_information.count);
+
   var msg = {};
-  if (invite_information.count === 1) {
+  if (countFound === 1) {
     var link_one = `mailto:${invite_information.sender_email}?subject=Responding%20to%20Your%20Invitation!&body=Hi%20there%2C%20%24%7B${invite_information.sender_name}%7D%2C%0D%0A%0D%0AI%20am%20pleased%20to%20accept%20your%20invitation%20to%20meet%20on%20%24%7B${invite_information.date_1}%7D%20at%20%24%7B${invite_information.time_1}%7D`;
     msg = {
       to: invite_information.email,
@@ -16,16 +18,14 @@ async function sendInvite(invite_information) {
       dynamic_template_data: {
         subject: invite_information.subject,
         sender_name: invite_information.sender_name,
-        sender_email: invite_information.sender_email,
         event_name: invite_information.event_name,
         name: invite_information.name,
         date_1: invite_information.date_1,
         time_1: invite_information.time_1,
-        event_link_1: link_one,
-        decline_link: `https://grabbin-coffee-frontend.now.sh/decline/${invite_information.invite_id}`
+        event_link_1: invite_information.event_link_1
       }
     };
-  } else if (invite_information.count === 2) {
+  } else if (countFound === 2) {
     msg = {
       to: invite_information.email,
       from: "gegarnhart@email.wm.edu",
@@ -37,14 +37,14 @@ async function sendInvite(invite_information) {
         name: invite_information.name,
         date_1: invite_information.date_1,
         time_1: invite_information.time_1,
-        event_link_1: `https://grabbin-coffee-frontend.now.sh/attend/1/${invite_information.invite_id}`,
+        event_link_1: invite_information.event_link_1,
         date_2: invite_information.date_2,
         time_2: invite_information.time_2,
-        event_link_1: `https://grabbin-coffee-frontend.now.sh/attend/2/${invite_information.invite_id}`,
-        decline_link: `https://grabbin-coffee-frontend.now.sh/decline/${invite_information.invite_id}`
+        event_link_2: invite_information.event_link_2,
+        decline_link: invite_information.decline_link
       }
     };
-  } else if (invite_information.count === 3) {
+  } else if (countFound === 3) {
     msg = {
       to: invite_information.email,
       from: "gegarnhart@email.wm.edu",
@@ -60,13 +60,13 @@ async function sendInvite(invite_information) {
         date_2: invite_information.date_2,
         time_2: invite_information.time_2,
         event_link_2: invite_information.event_link_2,
-        event_link_3: invite_information.event_link_3,
         date_3: invite_information.date_3,
         time_3: invite_information.date_3,
-        decline_link: "https://sadpuppy.games/"
+        event_link_3: invite_information.event_link_3,
+        decline_link: invite_information.decline_link
       }
     };
-  } else if (invite_information.count === 4) {
+  } else if (countFound === 4) {
     msg = {
       to: invite_information.email,
       from: "gegarnhart@email.wm.edu",
@@ -88,10 +88,10 @@ async function sendInvite(invite_information) {
         date_4: invite_information.date_4,
         time_4: invite_information.time_4,
         event_link_4: invite_information.event_link_4,
-        decline_link: "https://sadpuppy.games/"
+        decline_link: invite_information.decline_link
       }
     };
-  } else if (invite_information.count === 5) {
+  } else if (countFound === 5) {
     msg = {
       to: invite_information.email,
       from: "gegarnhart@email.wm.edu",
@@ -108,7 +108,7 @@ async function sendInvite(invite_information) {
         time_2: invite_information.time_2,
         event_link_2: invite_information.event_link_2,
         date_3: invite_information.date_3,
-        time_3: invite_information.date_3,
+        time_3: invite_information.time_3,
         event_link_3: invite_information.event_link_3,
         date_4: invite_information.date_4,
         time_4: invite_information.time_4,
@@ -116,47 +116,23 @@ async function sendInvite(invite_information) {
         date_5: invite_information.date_5,
         time_5: invite_information.time_5,
         event_link_5: invite_information.event_link_5,
-        decline_link: "https://sadpuppy.games/"
+        decline_link: invite_information.decline_link,
+        location: invite_information.location
       }
     };
   }
-
-  await sgMail.send(msg);
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.log("Error in sending mail!");
+    console.log(error);
+  }
 }
 
 module.exports = async (req, res) => {
-  console.log("MADE IT BABY");
-  console.log(req.body.email);
-  var invite_information = {
-    invite_id: "",
-    email: req.body.email,
-    sender_email: "greg.garnhart12@gmail.com",
-    count: 1,
-
-    subject: "Greg is inviting you to get Coffee from the Android App!",
-    sender_name: "Greg",
-    event_name: "Example Event",
-    name: "Reid",
-    date_1: "February 20, 2020",
-    time_1: "12:00PM EST",
-    event_link_1: "https://reddit.com",
-    date_2: "February 20, 2020",
-    time_2: "12:00PM EST",
-    event_link_2: "https://reddit.com",
-    date_3: "February 20, 2020",
-    time_3: "12:00PM EST",
-    event_link_3: "https://reddit.com",
-    date_4: "February 20, 2020",
-    time_4: "12:00PM EST",
-    event_link_4: "https://reddit.com",
-    date_5: "February 20, 2020",
-    time_5: "12:00PM EST",
-    event_link_5: "https://reddit.com",
-    decline_link: "https://sadpuppy.games/"
-  };
-
+  console.log(req.body);
   try {
-    await sendInvite(invite_information);
+    await sendInvite(req.body);
     res.send();
   } catch (error) {
     console.log(error);
